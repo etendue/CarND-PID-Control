cc08f6bd78e735b4c56e867a906ea75c8ade0ad3
Yusun Liu <liuyusun@hotmail.com>
2017-06-09 20:06:29 +0200

    Feat: add move function for Car
    apply motion model

--- src/Car.cpp
+++ src/Car.cpp
@@ -7,7 +7,9 @@
 
 #include <math.h>
 #include "Car.h"
+using namespace std;
 
+static const double max_steering_angle = M_PI/4;
 
 Car::Car(double length) {
   x_=0;
@@ -25,5 +27,29 @@ void Car::init(double x, double y, double orientation) {
   orientation_=orientation % (2*M_PI);
 }
 
-void Car::move(double speed, double steering) {
+void Car::move(double speed, double steering, double t,const double tolerance) {
+	//check steering angle boundary
+	double steering_abs = min(fabs(steering),max_steering_angle);
+	steering = copysign(steering_abs,steering);
+
+	//check the distance boundary
+	double distance = max(0.0,speed*t);
+
+	//add noise if necessary
+	//the turn angle based on bicycle mode
+	double turn_angle = tan(steering) * distance / length_;
+
+	if (fabs(turn_angle) < tolerance) {
+		// approximate by straight line motion
+		x_ += distance * cos(orientation_);
+		y_ += distance * sin(orientation_);
+		orientation_ = (orientation_ + turn_angle) % (2.0 * M_PI);
+	} else {
+		// approximate bicycle model for motion
+		double radius = distance / turn_angle;
+		x_ += radius * (sin(orientation_ + turn_angle) - sin(orientation_));
+		y_ += radius * (cos(orientation_ + turn_angle) - cos(orientation_));
+		orientation_ = (orientation_ + turn_angle) % (2.0 * M_PI);
+	}
+
 }
--- src/Car.h
+++ src/Car.h
@@ -10,14 +10,15 @@
 
 class Car {
  public:
-  Car(double length=4);
+  Car(double length=2.6);
   virtual ~Car();
   void init(double x,double y, double orientation);
-  void move(double speed,double steering);
- private:
+  void move(double speed,double steering,double t,const double tolerance=0.001);
+
   double x_;
   double y_;
   double orientation_;
+private:
   double length_;
 };
 
